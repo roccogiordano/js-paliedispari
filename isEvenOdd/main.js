9a32982db6709f3bb4ea9d1ea76d0a3d461404eb
roccogiordano <rocco.giordano573@gmail.com>
2024-10-21 22:44:58 +0200

    feat: variables initialization and functions declaration

---
+++ isEvenOdd/main.js
@@ -0,0 +1,51 @@
+// Variables Initialization
+
+const userChoice = prompt("Scegli pari o dispari:").toLowerCase();
+const userNum = parseInt(prompt("Scegli un numero da 1 a 5:"));
+const rndNumber = getRndInteger(1, 5);
+let sumNum = undefined;
+
+//
+
+
+// Functions Declaration
+
+/**
+ * Checks if a given number is even or odd.
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {string}
+ */
+
+function isEvenOdd(num1, num2) {
+
+    sumNum = num1 + num2;
+
+    if (sumNum % 2 === 0) {
+
+        result = "pari";
+
+    } else {
+
+        result = "dispari";
+
+    }
+
+    return result;
+
+};
+
+/**
+ * Gives a random number between 1 and 5.
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
+
+function getRndInteger(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+
+};
+
+//
